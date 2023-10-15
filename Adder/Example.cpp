1e91ead51d13d31d68a4611d0f343dce538dc5ea
Katechkin Nikita <nikibot4mac@outlook.com>
2023-10-15 16:59:49 +0300

    Fixed code and CT

--- Adder/Example.cpp
+++ Adder/Example.cpp
@@ -1,5 +1,5 @@
 #include "Example.hpp"
 
 int Calculator::getNumberNine() {
-    return 9;
+    return 11;
 }
--- tests/ExampleTests.cpp
+++ tests/ExampleTests.cpp
@@ -12,7 +12,7 @@ TEST(Hello, world) {
 
 TEST(Hello, world1) {
     std::cout << "New test with lib!" << std::endl;
-    EXPECT_EQ(Calculator::getNumberNine(), 7);
+    EXPECT_EQ(Calculator::getNumberNine(), 11);
 }
 
 int main(int argc, char** argv)
